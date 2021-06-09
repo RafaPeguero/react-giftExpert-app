e8e2353f10f0aac452e1d2fb56a5a3df55fb3369
Rafael Peguero <rafaelpeguero9816@gmail.com>
2021-06-08 21:15:32 -0600

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,23 @@
+import { useState,useEffect } from "react"
+import { getGif } from '../helpers/getGifs';
+
+
+export const useFetchGifs = (category) => {
+    const [state, setstate] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect(()=> {
+        getGif(category)
+        .then( img => {
+
+            setstate({
+            data: img,
+            loading: false
+            });     
+        })
+      },[category]);
+
+    return state;
+} 
